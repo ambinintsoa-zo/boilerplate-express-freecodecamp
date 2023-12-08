2c83291a8b6b7b8a527ad69e889252c769b5e9a2
Ambinintsoa Herizo <80875781+ambinintsoa-zo@users.noreply.github.com>
2023-12-08 11:25:55 +0000

    Update 3 files

--- myApp.js
+++ myApp.js
@@ -1,39 +1,23 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 
+require("dotenv").config();
 
+app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/", (req, res) => {
+  absolutePath = __dirname + "/views/index.html";
+  res.sendFile(absolutePath);
+});
 
+app.get("/json", (req, res) => {
+  let response = "Hello json";
 
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    response = response.toUpperCase();
+  }
 
+  res.json({ message: response });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
